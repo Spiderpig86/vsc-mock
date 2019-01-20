f2579575a4276a1faeb418d004cf960e0677dfc2
Spiderpig86 <slim679975@gmail.com>
2019-01-20 00:26:07 -0500

    feat(time.ts): Added command with options for the time category

--- src/categories/time.ts
+++ src/categories/time.ts
@@ -42,34 +42,20 @@ export class Time implements ICategory {
     public async execHandlers(command: string): Promise<any> {
         switch (command) {
             case '.date': {
-                
+                return await this.handleDate();
             }
-            case '.date': {
-                
+            case '.hour': {
+                return await this.handleHour();
             }
-            case '.date': {
-                
+            case '.weekday': {
+                return await this.handleWeekday();
             }
-            case '.date': {
-                
-            }
-            case '.date': {
-                
-            }
-            case '.date': {
-                
-            }
-            case '.date': {
-                
-            }
-            case '.date': {
-                
-            }
-            case '.date': {
-                
+            case '.year': {
+                return await this.handleYear();
             }
             default: {
-
+                console.log('No command found');
+                return null;
             }
         }
     }
@@ -80,7 +66,7 @@ export class Time implements ICategory {
             'Use American date format? (y/n)',
             'Enter a year (eg. 1983, blank for random)'
         ]);
-        const val = this.chance.coordinates({
+        const val = this.chance.date({
             ...values[0] && { string: values[0].toLowerCase() === 'y' },
             ...values[1] && { american: values[1].toLowerCase() === 'y' },
             ...values[2] && { year: Number(values[2]) },
@@ -90,15 +76,35 @@ export class Time implements ICategory {
 
     public async handleHour(): Promise<any> {
         const values = await displayPrompts([
-            'Use string variant of date (MM/DD/YYY)? (y/n)',
-            'Use American date format? (y/n)',
-            'Enter a year (eg. 1983, blank for random)'
+            'Use 24 hour format? (y/n)'
         ]);
-        const val = this.chance.coordinates({
-            ...values[0] && { string: values[0].toLowerCase() === 'y' },
-            ...values[1] && { american: values[1].toLowerCase() === 'y' },
-            ...values[2] && { year: Number(values[2]) },
+        const val = this.chance.hour({
+            ...values[0] && { twentyfour: values[0].toLowerCase() === 'y' },
         });
         return val;
     }
+
+    public async handleWeekday(): Promise<any> {
+        const values = await displayPrompts([
+            'Show weekday only? (y/n)'
+        ]);
+        const val = this.chance.weekday({
+            ...values[0] && { weekday_only: values[0].toLowerCase() === 'y' },
+        });
+        return val;
+    }
+
+    public async handleYear(): Promise<any> {
+        const values = await displayPrompts([
+            'Enter min year (eg. 1900 or blank for none)',
+            'Enter max year (eg. 2100 or blank for none)'
+        ]);
+        const val = this.chance.year({
+            ...values[0] && { min: values[0] },
+            ...values[1] && { max: values[1] },
+        });
+        return val;
+    }
+
+
 }
