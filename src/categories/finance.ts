1edf9edcb24fa0a0214aa7df9ac18dd0dc5e9beb
Spiderpig86 <slim679975@gmail.com>
2019-01-24 18:30:27 -0500

    feat(finance.ts): Added commands for finance category

--- src/categories/finance.ts
+++ src/categories/finance.ts
@@ -1,5 +1,7 @@
 import { Chance } from 'chance';
+
 import { ICategory } from './category.interface';
+import { displayPrompts } from '../consts/options';
 
 export class Finance implements ICategory {
 
@@ -38,6 +40,89 @@ export class Finance implements ICategory {
     }
 
     public async execHandlers(command: string): Promise<any> {
+        switch (command) {
+            case '.cc': {
+                return await this.handleCc();
+            }
+            case '.cc_type': {
+                return await this.handleCcType();
+            }
+            case '.dollar': {
+                return await this.handleDollar();
+            }
+            case '.euro': {
+                return await this.handleEuro();
+            }
+            case '.exp': {
+                return await this.handleExp();
+            }
+            case '.exp_month': {
+                return await this.handleExpMonth();
+            }
+            default: {
+                console.log('No command found');
+                return null;
+            }
+        }
+    }
 
+    public async handleCc(): Promise<any> {
+        const values = await displayPrompts([
+            'Enter card type (eg. Mastercard or mc (short))'
+        ]);
+        const val = this.chance.cc({
+            ...values[0] && { type: values[0] }
+        });
+        return val;
+    }
+
+    public async handleCcType(): Promise<any> {
+        const values = await displayPrompts([
+            'Return raw JSON object? (y/n)'
+        ]);
+        const val = this.chance.cc_type({
+            ...values[0] && { raw: values[0].toLowerCase() === 'y' }
+        });
+        return val;
+    }
+
+    public async handleDollar(): Promise<any> {
+        const values = await displayPrompts([
+            'Enter max limit (eg. 250)'
+        ]);
+        const val = this.chance.dollar({
+            ...values[0] && { max: Number(values[0]) }
+        });
+        return val;
+    }
+
+    public async handleEuro(): Promise<any> {
+        const values = await displayPrompts([
+            'Enter max limit (eg. 250)'
+        ]);
+        const val = this.chance.euro({
+            ...values[0] && { max: Number(values[0]) }
+        });
+        return val;
+    }
+
+    public async handleExp(): Promise<any> {
+        const values = await displayPrompts([
+            'Return raw JSON object? (y/n)'
+        ]);
+        const val = this.chance.exp({
+            ...values[0] && { raw: values[0].toLowerCase() === 'y' }
+        });
+        return val;
+    }
+
+    public async handleExpMonth(): Promise<any> {
+        const values = await displayPrompts([
+            'Use a future month? (y/n)'
+        ]);
+        const val = this.chance.exp_month({
+            ...values[0] && { future: values[0].toLowerCase() === 'y' }
+        });
+        return val;
     }
 }
